580b99e9b8ab3a073ff9d4ed9b8fb182c58c5767
bodarey <69037258+bodarey@users.noreply.github.com>
2024-03-28 19:38:03 +0000

    lesson 11

---
+++ 11_iterators/app.rb
@@ -0,0 +1,4 @@
+(1..7).select { |x| x.even? }.reduce do |m, x|
+puts "debug: #{x}"
+m + x
+end
---
+++ 11_iterators/app1.rb
@@ -0,0 +1,12 @@
+arr = []
+while 2 + 2 == 4
+	puts "add a color"
+	answer = gets.strip
+	break  if answer.downcase == 'stop'
+	arr << answer
+
+
+
+end
+puts '---------------'
+puts arr
