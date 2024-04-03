7b45a8e527787ea7f5fb0fe1d8a77439ef133c67
bodarey <69037258+bodarey@users.noreply.github.com>
2024-04-03 22:44:41 +0100

    homework 12_6

---
+++ 12_blocks/app6.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'date'
+
+def week(n)
+  arr = []
+  1.upto(7) do |x|
+    dd = Date.commercial(Time.new.year, n.to_i, x)
+    arr << dd
+    yield  dd if block_given?
+  end
+  arr
+end
+week(50) {|i| puts i }
