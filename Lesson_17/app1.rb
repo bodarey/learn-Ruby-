a9a553b6a8206e1fdd1b169b0c6361883012aad9
bodarey <69037258+bodarey@users.noreply.github.com>
2024-04-24 10:18:33 +0100

    Update app1.rb

--- Lesson_17/app1.rb
+++ Lesson_17/app1.rb
@@ -1,59 +1,44 @@
 class Vessel
-  attr_accessor :rockets
+  attr_accessor :rockets, :torpeds, :cargo_hold, :crane
 
   def initialize(options)
     @rockets = options[:rockets]
+    @torpeds = options[:torpeds]
+    @cargo_hold = options[:cargo_hold]
+    @crane = options[:crane]
+  end
+
+  def to_s
+    puts "rockets: #{@rockets || 0}, torpeds: #{@torpeds || 0} cargo_hold: #{@cargo_hold || 0} crane: #{@crane || 0}"
   end
 end
 
 ###################################
 class Underwater < Vessel
-  attr_accessor :torpeds
-
   def initialize(options)
     super options
-    @torpeds = options[:torpeds]
   end
 end
 
 ###################################
 class OnWater < Vessel
-  attr_accessor :cargo_hold, :crane
-
-  def initialize(options)
-    @cargo_hold = options[:cargo_hold]
-    @crane = options[:crane]
-  end
-end
-
-###################################
-class AtomSubmarine < Underwater
   def initialize(options)
     super options
   end
-
-  def to_s
-    puts "rockets: #{@rockets || 0}, torpeds: #{@torpeds || 0}"
-  end
 end
+atom_submarine = Underwater.new rockets:10, torpeds:9
+dry_cargo_grain = OnWater.new cargo_hold: 1, creane: 1
+container_transporter =  OnWater.new crane: 1
+oil_tanker = OnWater.new cargo_hold: 2
+rocket_cruiser = OnWater.new rockets: 30
+military_transport = OnWater.new rockets: 10, cargo_hold: 5, crane: 1
+puts atom_submarine
+puts dry_cargo_grain
+puts container_transporter
+puts oil_tanker
+puts rocket_cruiser
+puts military_transport
 
-#################################
-class DryVesselGrain < OnWater
-  def initialize(options)
-    super options
-  end
-undef_method :rockets
-end
-#################################
-class ContainerTransporter
-def initialize  options
-super options
-undef_method :rockets
-undef_method :cargo_hold
-end
 
-#################################
-#################################
 
-u = AtomSubmarine.new torpeds: 10
-puts u
+#puts u
