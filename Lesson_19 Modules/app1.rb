469460948e5162fece8674242e949592464a963e
bodarey <69037258+bodarey@users.noreply.github.com>
2024-07-26 19:14:02 +0100

    Lesson_19 modules

---
+++ Lesson_19 Modules/app1.rb
@@ -0,0 +1,27 @@
+class Vector
+  def initialize(a, b)
+    @a = a
+    @b = b
+  end
+
+  def distance
+    Math.sqrt( ((@b.x - @a.x).pow(2)) + ((@b.y - @a.y).pow(2)))
+  end
+
+  class Point
+    attr_accessor :x, :y
+
+    def initialize(x, y)
+      @x = x
+      @y = y
+    end
+  end
+
+  # d=√((x2 – x1)² + (y2 – y1)²
+end
+
+a = Vector::Point.new 0, 0
+b = Vector::Point.new 7, 0
+
+v = Vector.new a, b
+puts v.distance
