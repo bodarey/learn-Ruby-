f7de0ccc3641f665195ee69410293286784e25ec
bodarey <69037258+bodarey@users.noreply.github.com>
2024-07-30 19:29:06 +0100

    Create app2.rb

---
+++ Lesson_20 Modules/app2.rb
@@ -0,0 +1,14 @@
+COLORS = {
+red: 'красный',
+orange: 'оранжевый',
+yellow: 'желтый',
+green: 'зеленый',
+blue: 'голубой',
+indigo: 'синий',
+violet: 'фиолетовый'
+}
+def method_missing color
+   COLORS[color]
+end
+
+puts violet
