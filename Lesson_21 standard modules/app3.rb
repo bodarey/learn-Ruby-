7456ac9e946d0658a0855fa440d0e9d44a99c45c
bodarey <69037258+bodarey@users.noreply.github.com>
2024-08-06 21:57:02 +0100

    test

--- Lesson_21 standard modules/app3.rb
+++ Lesson_21 standard modules/app3.rb
@@ -1,4 +1,14 @@
+require 'json'
 class Hash
-   def to_json
-   end
+  # def to_json
+  # end
 end
+h = {'a' => [1,2,3], :b => {:a => 5} }
+
+person = {
+first_name: 'Иван',
+last_name: 'Петрович',
+patronymic: 'Сидоров'
+}
+#puts person.to_json.inspect 
+puts h.to_json.inspect 
