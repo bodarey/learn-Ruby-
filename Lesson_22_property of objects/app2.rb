31a0db276df9d8ab80d2b9bedb70be570723f27d
bodarey <69037258+bodarey@users.noreply.github.com>
2024-08-12 19:33:24 +0100

    Update app2.rb

--- Lesson_22_property of objects/app2.rb
+++ Lesson_22_property of objects/app2.rb
@@ -1,24 +1,24 @@
 class Text
-
-  def initialize n = 3
+  def initialize(n = 3)
     @n = n
     @list = Hash.new(0)
   end
- def getText
-    1.upto(@n) do 
+
+  def getText
+    rez = ''
+    1.upto(@n) do
       str = gets.strip
-       
-       
-      
-      @list[str]+=1 unless str.match?(/[^A-z]/)
-      
+      @list[str] += 1 unless str.match?(/[^A-z]/)
     end
- arr = @list.to_a
- arr.sort!{|a,b| a[0]<=>b[0]}
- arr
- end
+    arr = @list.to_a
+    arr.sort! { |a, b| a[0] <=> b[0] }
+    arr.each do |x|
+      rez += x[1] < 2 ? "\n#{x[0]}" : "\n#{x[0]}(#{x[1]})"
+    end
+    rez
+  end
 end
 
-t = Text.new 
+t = Text.new 10
 
-puts t.getText.inspect
+puts t.getText
