804ed5cb9d9ef1af5b5bcce94913fbe22fdebb11
bodarey <69037258+bodarey@users.noreply.github.com>
2024-08-18 19:29:39 +0100

    Lesson 23 Hash

---
+++ Lesson_24 Hash/app1.rb
@@ -0,0 +1,15 @@
+f = File.open('countries.txt', 'r+')
+arr = []
+while(line = f.gets) do
+  ll = line[/.+%/]  # cutting the string until %
+  l = ll.rpartition(/.* .* /)  # cutting from the right through spaces
+  first = l[0].rpartition(/( T |T |C |\d)/)  # from result array cutting first element, so will remain only the name of country
+  l[0] = first.last # last element from first divided array
+ # puts l.inspect
+  arr << [l[0].strip,l[1].strip.split(',').join('').to_i]
+
+end
+hh = arr.to_h
+p hh
+
+f.close
