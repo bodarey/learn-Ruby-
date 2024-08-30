b835bacf91179add065f171c9e3044626701f30f
bodarey <69037258+bodarey@users.noreply.github.com>
2024-08-30 22:32:01 +0100

    Update app4.rb

--- Lesson_25 struct /app4.rb
+++ Lesson_25 struct /app4.rb
@@ -9,17 +9,22 @@ class Ship
     @m = 10
     # arr[1][3]= 0
     if position == :o
-      y.upto(y + sizze - 1) do |i|
+    i =y
+    #while i <= (y+(sizze-1)) 
+      y.upto(y + (sizze - 1)) do |i|
+puts "--#{i}---------"
         # puts arr[x][i].inspect
         # checking on horizontal line from left to right
         if (arr[x][i] != @condition) || (x == 1 ? false : arr[x - 1][i] != @condition) || (x == 10 ? false : arr[x + 1][i] != @condition)
           return false
         end
+      i+=1
       end
       # checking the left and right side of the ship
+
       if (y == 1 ? false : (arr[x][y - 1] != @condition)) || (y + sizze == @n + 1 ? false : arr[x][y + sizze] != @condition)
-        return false
-      end
+       return false
+     end
       # checking top left corner
       return false if x == 1 && y == 1 && arr[x + 1][y + sizze] != @condition
 
@@ -28,23 +33,24 @@ class Ship
       # checking left botom corner
       return false if x > 1 && (x == @m && y == 1 && arr[x - 1][y + sizze] != @condition)
       # checking right bottom corner
-      return false if x > 1 and y > 1 && (x == @m && y + sizze - 1 == @n or arr[x - 1][y - 1] != @condition)
-      if y > 1 && (x == 1 && arr[x + 1][y + sizze] != @condition or arr[x + 1][y - 1] != @condition)
+      return false if x > 1 and y > 1 && (x == @m && (y + sizze - 1 > @n or arr[x - 1][y - 1] != @condition))
+
+      if y > 1 && y+sizze<@n && (x == 1 && arr[x + 1][y + sizze] != @condition or arr[x + 1][y - 1] != @condition)
         # ship is on the top checking intersection corners on right and left bottom
         return false
       end
       # ship is on the bottom checking intersection on left and right top
-      if x > 1 and y > 1 && (x == @n && arr[x - 1][y - 1] != @condition or arr[x - 1][y + sizze] != @condition)
+      if x > 1 and  y+sizze < @m and y > 1 && (x == @n && arr[x - 1][y - 1] != @condition or arr[x - 1][y + sizze] != @condition)
         return false
       end
       # ship is on the left checking the intersection right top and bottom
-      return false if x > 1 && (y == 1 && arr[x - 1][y + sizze] != @condition or arr[x + 1][y + sizze] != @condition)
+      return false if x > 1 && x<@n && (y == 1 && arr[x - 1][y + sizze] != @condition or arr[x + 1][y + sizze] != @condition)
       # ship is on the right checking intersection top left
       return false if x > 1 and y > 1 && (y == @m && arr[x - 1][y - 1] != @condition)
       # ship is on the right checking intersection bottom left
-      return false if y > 1 && (y == @m && arr[x + 1][y - 1] != @condition)
+      return false if y > 1 && x<@n && (y == @m && arr[x + 1][y - 1] != @condition)
       # checking around the ship on intersection when ship is not on margins
-      if x > 1 and y > 1 && (arr[x - 1][y - 1] != @condition or arr[x + 1][y - 1] != @condition or arr[x - 1][y + sizze] != @condition or arr[x + 1][y + sizze] != @condition)
+      if x > 1 and y > 1 && x<@n && (arr[x - 1][y - 1] != @condition or arr[x + 1][y - 1] != @condition or arr[x - 1][y + sizze] != @condition or arr[x + 1][y + sizze] != @condition)
         return false
       end
 
@@ -114,9 +120,10 @@ end
 t = Table.new
 t.show_matrix
 t.a = Ship.create t.a, 1, 1, 1, :o
-t.a = Ship.create t.a, 1, 3, 1, :o
-t.a = Ship.create t.a, 2, 7, 1, :o
-# t.a = Ship.create t.a,3,8,2,:o
+t.a = Ship.create t.a, 1, 10, 1, :o
+t.a = Ship.create t.a, 10, 1, 1, :o
+t.a = Ship.create t.a, 10, 10, 1, :o
+ t.a = Ship.create t.a,4,8,2,:o
 
 t.show_matrix
 
