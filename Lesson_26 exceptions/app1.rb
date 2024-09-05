b65ce265d88fbdfbaa75111e05cc019e2f6d44a6
bodarey <69037258+bodarey@users.noreply.github.com>
2024-09-05 19:29:26 +0100

    Update app1.rb

--- Lesson_26 exceptions/app1.rb
+++ Lesson_26 exceptions/app1.rb
@@ -3,12 +3,13 @@ end
 
 class User
   def initialize(first_name, last_name, email)
-    raise UserException.new('letters for name are incorect') unless (first_name.match(/[a-zA-Z]/) and last_name.match(/[a-zA-Z]/) and email.match(/@/) )
+    raise UserException.new(' name and/or email are incorect') unless (first_name.match(/[a-zA-Z]/) and last_name.match(/[a-zA-Z]/) and email.match(/@.+\./) )
   end
 end
 
+puts 'please insert first name, last name and e-mail'
 firstname = gets.strip
 lastname = gets.strip
-email = 'email@email.com'
+email = gets.strip
 
 obj = User.new firstname, lastname, email
