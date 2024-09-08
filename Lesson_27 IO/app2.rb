a624934d3626e1254fa723df92eae55227745f64
bodarey <69037258+bodarey@users.noreply.github.com>
2024-09-08 19:07:03 +0100

    app3

---
+++ Lesson_27 IO/app2.rb
@@ -0,0 +1,5 @@
+file = ARGV[0] || 'test.txt'
+arr = File.readlines(file).shuffle
+File.open(file,'w') do  |f|
+  f.puts arr[0]
+end           
---
+++ Lesson_27 IO/app3.rb
@@ -0,0 +1,27 @@
+#puts File.size('text.txt')
+
+a = ARGV
+arr = ('1'..'10').include?(a[0][0]) ? (a) : ([a[1],a[0]])
+
+puts arr
+file = arr[1]
+size = arr[0].to_i
+
+f = File.new(file,'w') 
+
+loop do
+break if f.size >= size
+f.write 'a' 
+end
+
+f.close
+
+
+
+
+
+
+
+
+
+              
