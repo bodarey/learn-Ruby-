2fbcd5d34df9eb142ee13755f2a6644da7c81e02
bodarey <69037258+bodarey@users.noreply.github.com>
2024-09-16 15:15:57 +0100

    refactoring

--- Lesson_27 IO/app5.rb
+++ Lesson_27 IO/app5.rb
@@ -1,6 +1,6 @@
 file = ARGV[0] || 'test.txt'
 size = lambda do |f|
-s = File.size(f)
+  s = File.size(f)
   result = if s > 1024 * 1024
              "#{s.to_f / (1024 * 1024)} Mb"
            elsif s > 1024
--- Lesson_27 IO/app6/app.rb
+++ Lesson_27 IO/app6/app.rb
@@ -11,7 +11,7 @@ if File.size(f) > 10
     arr << str[0...n]
     str = str[n..-1]
   end
-  arr << str          
+  arr << str
   1.upto(10) do |i|
     name = i < 10 ? "f_x0#{i}.txt" : 'f_x10.txt'
     f = File.new(name, 'w')
