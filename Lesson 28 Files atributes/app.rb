e5919d8b497dbddeb266766a7c195f0ed393d001
bodarey <69037258+bodarey@users.noreply.github.com>
2024-09-18 19:46:34 +0100

    Lesson 28

--- Lesson 28 Files atributes/app.rb
+++ Lesson 28 Files atributes/app.rb
@@ -1,25 +1,36 @@
+def get_subfolders(path = Dir.pwd)
+  # g = File.absolute_path(File.basename Dir.pwd)
+  @main_path = path
+  # puts g
+  # puts path
+  @arr = []
+  @data = []
 
-def get_subfolders path = Dir.pwd
-#g = File.absolute_path(File.basename Dir.pwd)
+  def get_folder(path) # return a hash with folders
+    Dir.chdir(path)
+    @arr << File.basename(Dir.pwd)
 
-#puts g
-#puts path
-@arr = []
+    Dir.glob('*').each do |f|
+      next unless File.directory? f
 
-	def get_folder path           
-        	Dir.chdir(path)
-                @arr << File.basename(Dir.pwd)
-                Dir.glob('*').each do |f|
-                 if File.directory? f              
-                                         get_folder File.absolute_path f
-                   Dir.chdir("..") 
-		 end
-                end
+      get_folder File.absolute_path f
+      Dir.chdir('..')
+      if Dir.pwd == @main_path
+        @data << @arr
+        @arr = []
+      end
+    end
+  end
 
-	end
-               get_folder path
-{'main_folder'=>@arr.shift,'folders'=>@arr}
+  def get_result
+    base =  @data[0].shift
+    folders = @data.map { |x| x[0] }
+    subfolders = @data.map { |x| { x.shift => x } }
+    { 'main_folder' => base, 'folders' => folders, 'subfolders_of_folders' => subfolders }
+  end
+
+  get_folder path
+  get_result
 end
 
-
-puts get_subfolders '/home/andy/Desktop/Ruby-ROR-IlyaBodrovKrukowski/Ruby с нуля'
+puts(get_subfolders) # '/home/andy/Desktop/Ruby-ROR-IlyaBodrovKrukowski/Ruby с нуля'
