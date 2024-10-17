a60d1fcc4601c6d3bc34283fc4aa334ed5c8b80d
bodarey <69037258+bodarey@users.noreply.github.com>
2024-10-17 11:48:26 +0100

    Update app3.rb

--- Lesson 29 Directories/app3.rb
+++ Lesson 29 Directories/app3.rb
@@ -2,11 +2,21 @@ def folder_details path
 @count = 0
 	def get_files path
          Dir.chdir path
-	 puts "#{' ' * @count}" + Dir.entries(path).reject{|x| %w[. ..].include? x}.to_s
+	 directory = File.basename(Dir.getwd)
+         files = Dir.entries(path).reject{|x| %w[. ..].include? x}
+	 puts "#{'-'*@count}-> #{' ' * @count}#{directory}\n\t#{'  ' * @count*2}" + files.to_s
 	 @count += 1
+	 files.each do |x| 
+		if File.directory? x
+                  get_files File.join(path, x)
+                  Dir.chdir '..'
+                  @count -=1
+                 
+		end
+ 	 end
 	end
 
 get_files path
 end
 	     
-folder_details Dir.pwd
+folder_details '/home/andy/Desktop/Ruby-ROR-IlyaBodrovKrukowski'
