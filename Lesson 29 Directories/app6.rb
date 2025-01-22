ffcb9db847b6c09d6c00be4a142143c126c43783
bodarey <69037258+bodarey@users.noreply.github.com>
2025-01-22 10:06:04 +0000

    Update app6.rb

--- Lesson 29 Directories/app6.rb
+++ Lesson 29 Directories/app6.rb
@@ -1,7 +1,11 @@
 require 'fileutils'
 
+
 def copy_folder a,b
 FileUtils.copy_entry a,b
-
 end
 
+a = '/home/andy/Desktop/learn-Ruby-Igori-Simdianov/Lesson 29 Directories/test'
+b = '/home/andy/Desktop/Jsexpert'
+
+copy_folder a,b
